375b8b1e89b7f884bfd5362f19b817c9a0cb9fde
simsam0502 <simransaigal@icloud.com>
2020-11-28 18:18:01 +0530

    js added

--- Ninja_blade_game/game.js
+++ Ninja_blade_game/game.js
@@ -1,13 +1,21 @@
 var c=0;
-$(document).keypress(function (){
-  if(event.key=="Enter"){
+$(document).keypress(function (event){
+  if(event.key==="Enter"){
+    $("h1").text("Score: "+c);
     if(!$(".ninja").hasClass("jump")){
     $(".ninja").addClass("jump");
-    setTimeout(function () { $(".ninja").removeClass("jump");},1000);
-    if(c==0){
-      setTimeout(function (){$(".blade").addClass("attack");},1000);
-      c++;
+    setTimeout(function () { $(".ninja").removeClass("jump");},500);
+    c++;
     }
+    if(c==1){
+      setTimeout(function (){$(".blade").removeClass("hidden").addClass("attack");},500);
+    }
+    }
+});
+setInterval(function (){ 
+  if(parseInt($(".blade").css("left"))>=425&&parseInt($(".blade").css("left"))<=475&&
+  parseInt($(".ninja").css("top"))>=225){
+    $("h1").text("Game Over, your Score is "+c);
+    $(".blade").removeClass("attack").addClass("hidden");
   }
-  }
-})
+},1)
